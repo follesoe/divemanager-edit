66e8e4674e923eeb75f14abe45499fd89ac82bbb
Jonas Follesø <jonas@follesoe.no>
2014-10-11 18:52:00 -0500

    Moved sqlite code to controller.

--- src/scripts/controllers/main.js
+++ src/scripts/controllers/main.js
@@ -1,11 +1,19 @@
 var fs = require("fs");
-//var file = '/Users/follesoe/.config/Suunto/Suunto\ DM4/1.1.14.3463/DM4.db';
+var moment = require('moment');
 var file = '/Users/follesoe/Desktop/DM4.db';
+var sqlite3 = require("sqlite3").verbose();
 
 'use strict';
 angular.module('suuntoDMEditor')
   .controller('main', function ($scope) {
-		$scope.message = "Hello World!";
 		$scope.file = file;
-		$scope.fileExists = fs.existsSync(file);
+		$scope.dives = [];
+
+		var db = new sqlite3.Database(file);
+		db.each("SELECT DiveId, StartTime, Duration, Mode, MaxDepth FROM Dive", function(err, row) {
+			row.StartTime = moment((row.StartTime - 621355968000000000)/10000);
+			$scope.dives.push(row);
+			$scope.$apply();
+		});
+		db.close();
 	});
