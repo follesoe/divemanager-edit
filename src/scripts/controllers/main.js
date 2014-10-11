9667b123ba078850acbbb4b632ab535a9d8f5cc6
Jonas Follesø <jonas@follesoe.no>
2014-10-11 18:41:37 -0500

    Add Angular.

--- src/scripts/controllers/main.js
+++ src/scripts/controllers/main.js
@@ -1,5 +1,11 @@
+var fs = require("fs");
+//var file = '/Users/follesoe/.config/Suunto/Suunto\ DM4/1.1.14.3463/DM4.db';
+var file = '/Users/follesoe/Desktop/DM4.db';
+
 'use strict';
 angular.module('suuntoDMEditor')
   .controller('main', function ($scope) {
 		$scope.message = "Hello World!";
+		$scope.file = file;
+		$scope.fileExists = fs.existsSync(file);
 	});
