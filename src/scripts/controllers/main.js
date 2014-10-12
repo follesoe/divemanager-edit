fa0c2d75091a02ca865705eca7d9fa76149cad61
Jonas Follesø <jonas@follesoe.no>
2014-10-11 20:16:23 -0500

    Basic functionality up and running.

--- src/scripts/controllers/main.js
+++ src/scripts/controllers/main.js
@@ -1,20 +1,68 @@
-var fs = require("fs");
+var fs = require('fs');
 var moment = require('moment');
-var file = '/Users/follesoe/Desktop/DM4.db';
+var _ = require('lodash');
 var sqlite3 = require("sqlite3").verbose();
+
+//	'~/.config/Suunto/Suunto\ DM4/1.1.14.3463/';
 
 'use strict';
 angular.module('suuntoDMEditor')
   .controller('main', function ($scope) {
-		$scope.file = file;
-		$scope.dives = [];
+		var db;
+		var dmDir = process.env.HOME + '/.config/Suunto/Suunto DM4';
 
-		var db = new sqlite3.Database(file);
-		db.each("SELECT DiveId, StartTime, Duration, Mode, MaxDepth FROM Dive", function(err, row) {
-			row.selected = false;
-			row.StartTime = moment((row.StartTime - 621355968000000000)/10000);
-			$scope.dives.push(row);
-			$scope.$apply();
-		});
-		db.close();
+		if (fs.existsSync(dmDir)) {
+			var files = fs.readdirSync(dmDir);
+			var versionDir = _(files).filter(function (f) {
+				var stat = fs.statSync(dmDir + '/' + f);
+				return stat.isDirectory();
+			}).first();
+			dmDir = dmDir + '/' + versionDir;
+		}
+
+		$scope.mode = 0;
+		$scope.note = '';
+		$scope.file = dmDir + '/DM4.db';
+		$scope.fileExists = fs.existsSync($scope.file);
+		$scope.dives = [];
+		$scope.selectedDives = [];
+
+		$scope.save = function () {
+			db = new sqlite3.Database($scope.file);
+			db.serialize(function () {
+				var stmt = db.prepare('UPDATE Dive SET Mode = (?), Note = (?) WHERE DiveId = (?)');
+				_($scope.dives).filter('selected').forEach(function (dive) {
+					dive.Mode = $scope.mode;
+					dive.Note = $scope.note;
+					stmt.run(dive.Mode, dive.Note,	 dive.DiveId);
+				});
+				stmt.finalize();
+			});
+			db.close();
+			alert('Changes committed!');
+		}
+
+		$scope.backup = function () {
+			if (!$scope.fileExists) {
+				return;
+			}
+			fs.createReadStream($scope.file).pipe(fs.createWriteStream(dmDir + '/DM4.backup.db'));
+		}
+
+		$scope.deselect = function () {
+			_($scope.dives).forEach(function (dive) {
+				dive.selected = false;
+			});
+		}
+
+		if ($scope.fileExists) {
+			db = new sqlite3.Database($scope.file);
+			db.each('SELECT DiveId, StartTime, Duration, Mode, MaxDepth, Note FROM Dive', function (err, row) {
+				row.selected = false;
+				row.StartTime = moment((row.StartTime - 621355968000000000)/10000).zone(0).format('DD.MM.YY hh:mm');
+				$scope.dives.push(row);
+				$scope.$apply();
+			});
+			db.close();
+		}
 	});
