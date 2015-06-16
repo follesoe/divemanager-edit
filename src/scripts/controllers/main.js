f28c44858003cf1dfcc76e0549943a1e1a4d9235
Jonas Follesø <jonas@follesoe.no>
2015-06-16 20:25:09 +0200

    Refactored dbaccess to node module and use ES6 Promises

--- src/scripts/controllers/main.js
+++ src/scripts/controllers/main.js
@@ -3,10 +3,11 @@ var moment = require('moment');
 var _ = require('lodash');
 var sqlite3 = require('sqlite3').verbose();
 var dbpath = require('./scripts/services/dbpath.js');
+var dbaccess = require('./scripts/services/dbaccess.js');
 
 'use strict';
 angular.module('suuntoDMEditor')
-  .controller('main', function ($scope, dbaccess) {
+  .controller('main', function ($scope) {
     var pathResult = dbpath.getPath();
 
     $scope.file = pathResult.path;
@@ -34,6 +35,7 @@ angular.module('suuntoDMEditor')
 
     if ($scope.fileExists) {
       dbaccess.getDives($scope.file).then(function (dives) {
+        console.log("Then!" + dives.length);
         $scope.dives = dives;
         if (dives.length > 0) {
           $scope.selectedDive = dives[0];
--- src/scripts/services/dbaccess.js
+++ src/scripts/services/dbaccess.js
@@ -2,37 +2,37 @@ var _ = require('lodash');
 var moment = require('moment');
 var sqlite3 = require("sqlite3").verbose();
 
-angular.module('suuntoDMEditor')
-  .service('dbaccess', function ($q) {
+var getDives = function (dbPath) {
+  var promise = new Promise(function (resolve, reject) {
+    var db = new sqlite3.Database(dbPath);
+    var dives = [];
 
-    this.getDives = function (dbPath) {
-      var deferred = $q.defer();
-      var db = new sqlite3.Database(dbPath);
-      var dives = [];
-
-      db.all('SELECT DiveId, StartTime, Duration, Mode, MaxDepth, Note, BottomTemperature, DiveNumberInSerie, Source FROM Dive ORDER BY StartTime DESC', function (err, rows) {
-        _(rows).each(function (row) {
-          row.Selected = false;
-          row.StartDate = moment((row.StartTime - 621355968000000000)/10000).zone(0).format('DD.MM.YYYY');
-          row.StartTime = moment((row.StartTime - 621355968000000000)/10000).zone(0).format('HH:mm');
-          row.Duration = moment.utc(0).add(row.Duration, 's').format('HH:mm:ss');
-          dives.push(row);
-        });
-        deferred.resolve(dives);
+    db.all('SELECT DiveId, StartTime, Duration, Mode, MaxDepth, Note, BottomTemperature, DiveNumberInSerie, Source FROM Dive ORDER BY StartTime DESC', function (err, rows) {
+      _(rows).each(function (row) {
+        row.Selected = false;
+        row.StartDate = moment((row.StartTime - 621355968000000000)/10000).zone(0).format('DD.MM.YYYY');
+        row.StartTime = moment((row.StartTime - 621355968000000000)/10000).zone(0).format('HH:mm');
+        row.Duration = moment.utc(0).add(row.Duration, 's').format('HH:mm:ss');
+        dives.push(row);
       });
+      resolve(dives);
+    });
 
-      db.close();
-      return deferred.promise;
-    };
-
-    this.saveDive = function (dbPath, dive) {
-      var db = new sqlite3.Database(dbPath);
-      db.serialize(function () {
-        var stmt = db.prepare('UPDATE Dive SET Mode = (?) WHERE DiveId = (?)');
-        stmt.run(dive.Mode, dive.DiveId);
-        stmt.finalize();
-      });
-      db.close();
-    }
-
+    db.close();
   });
+
+  return promise;
+}
+
+var saveDive = function (dbPath, dive) {
+  var db = new sqlite3.Database(dbPath);
+  db.serialize(function () {
+    var stmt = db.prepare('UPDATE Dive SET Mode = (?) WHERE DiveId = (?)');
+    stmt.run(dive.Mode, dive.DiveId);
+    stmt.finalize();
+  });
+  db.close();
+}
+
+module.exports.getDives = getDives;
+module.exports.saveDive = saveDive;
