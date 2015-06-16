ffa5ccc5cb8d4c3aedd3ecb57fd4beb1991794ee
Jonas Follesø <jonas@follesoe.no>
2015-06-16 19:47:50 +0200

    Use dbpath as node module

--- src/scripts/controllers/main.js
+++ src/scripts/controllers/main.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var moment = require('moment');
 var _ = require('lodash');
-var sqlite3 = require("sqlite3").verbose();
+var sqlite3 = require('sqlite3').verbose();
+var dbpath = require('./scripts/services/dbpath.js');
 
 'use strict';
 angular.module('suuntoDMEditor')
-  .controller('main', function ($scope, dbpath, dbaccess) {
+  .controller('main', function ($scope, dbaccess) {
     var pathResult = dbpath.getPath();
 
     $scope.file = pathResult.path;
--- src/scripts/services/dbpath.js
+++ src/scripts/services/dbpath.js
@@ -1,45 +1,43 @@
 var fs = require('fs');
 var _ = require('lodash');
 
-angular.module('suuntoDMEditor')
-  .service('dbpath', function () {
+var getFirstSubDirectory = function (fromDir) {
+  var content = fs.readdirSync(fromDir);
 
-    var getFirstSubDirectory = function (fromDir) {
-      var content = fs.readdirSync(fromDir);
+  var firstSub =
+    _(content)
+      .filter(function (f) { return fs.statSync(fromDir + f).isDirectory(); })
+      .reverse()
+      .first();
 
-      var firstSub =
-        _(content)
-          .filter(function (f) { return fs.statSync(fromDir + f).isDirectory(); })
-          .reverse()
-          .first();
+  return firstSub;
+}
 
-      return firstSub;
-    }
+var getPath = function () {
+  var missing = {
+    exists: false,
+    path: ''
+  };
 
-    this.getPath = function () {
-      var missing = {
-        exists: false,
-        path: ''
-      };
+  var suuntoDir = process.env.HOME + '/.config/Suunto/';
+  if (!fs.existsSync(suuntoDir)) { return missing; }
 
-      var suuntoDir = process.env.HOME + '/.config/Suunto/';
-      if (!fs.existsSync(suuntoDir)) { return missing; }
+  var dmDir = suuntoDir + getFirstSubDirectory(suuntoDir) + '/';
+  if (!fs.existsSync(dmDir)) { return missing; }
 
-      var dmDir = suuntoDir + getFirstSubDirectory(suuntoDir) + '/';
-      if (!fs.existsSync(dmDir)) { return missing; }
+  var buildDir = dmDir + getFirstSubDirectory(dmDir) + '/';
+  if (!fs.existsSync(buildDir)) { return missing; }
 
-      var buildDir = dmDir + getFirstSubDirectory(dmDir) + '/';
-      if (!fs.existsSync(buildDir)) { return missing; }
-      
-      var dbPath = buildDir + 'DM4.db';
+  var dbPath = buildDir + 'DM4.db';
 
-      if (fs.existsSync(dbPath)) {
-        return {
-          exists: true,
-          path: dbPath
-        };
-      } else {
-        return missing;
-      }
+  if (fs.existsSync(dbPath)) {
+    return {
+      exists: true,
+      path: dbPath
     };
-  });
+  } else {
+    return missing;
+  }
+}
+
+module.exports.getPath = getPath;
