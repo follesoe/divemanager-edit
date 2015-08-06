7a8f48927def4b553b629c1ddd5499f27c2da927
Jonas Follesø <jonas@follesoe.no>
2015-08-06 16:03:04 +0200

    Displays information about selected dive.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -70,13 +70,90 @@ var DiveList = component('DiveList', function (props) {
   );
 });
 
+var DiveValue = component('DiveValue', function (props) {
+  return (
+    <div className="dive-data">
+      <h2>{this.props.label}</h2>
+      <div>
+        <img src={this.props.icon} />
+        <h3>{this.props.value.valueOf()}</h3>
+        <h4>{this.props.unit}</h4>
+      </div>
+    </div>
+  );
+});
+
 var DiveDetails = component('DiveDetails', function (props) {
   var dive = props.dive;
-  function onClick() {
+  /*function onClick() {
     return dive.set('Mode', 3);
-  }
+  }*/
   return (
-    <h1 onClick={onClick}>{dive.get('MaxDepth')}</h1>
+    <section>
+      <h1>Selected Dive</h1>
+      <h2>Dive Mode</h2>
+      <div className="divemodes">
+        <label className="divemode">
+          <img src="images/modes/0.png" />
+          <input type="radio" value="0" />
+          <h3>air</h3>
+        </label>
+        <label className="divemode">
+          <img src="images/modes/1.png" />
+          <input type="radio" value="1" />
+          <h3>ean</h3>
+        </label>
+        <label className="divemode">
+          <img src="images/modes/2.png" />
+          <input type="radio" value="2" />
+          <h3>gauge</h3>
+        </label>
+        <label className="divemode">
+          <img src="images/modes/3.png" />
+          <input type="radio" value="3" />
+          <h3>free</h3>
+        </label>
+      </div>
+      <div className="dive-data-fields">
+        <DiveValue
+          label="Time In"
+          unit="h"
+          icon="images/timeicon.png"
+          value={dive.get('StartTime')} />
+
+        <DiveValue
+          label="Max Depth"
+          unit="m"
+          icon="images/depthicon.png"
+          value={dive.get('MaxDepth')} />
+
+        <DiveValue
+          label="Max Depth Temp"
+          unit="C"
+          icon="images/tempicon.png"
+          value={dive.get('BottomTemperature')} />
+
+        <DiveValue
+          label="Duration"
+          unit="h"
+          icon="images/timeicon.png"
+          value={dive.get('Duration')} />
+
+        <DiveValue
+          label="Dive In Series"
+          unit=""
+          icon="images/numbericon.png"
+          value={dive.get('DiveNumberInSerie')} />
+
+        <DiveValue
+          label="Computer"
+          unit=""
+          icon="images/computericon.png"
+          value={dive.get('Source')} />
+
+        <button className="action">Save Changes</button>
+      </div>
+    </section>
   );
 });
 
@@ -88,14 +165,14 @@ var DiveApp = component('DiveApp', function (props) {
   }) || appState.cursor('nullDive');
 
   return (
-    <section>
+    <div>
       <DiveList
         dives={appState.cursor('dives')}
         dbpath={appState.cursor('dbpath')}
         selectedDiveId={appState.cursor('selectedDiveId')} />
 
       <DiveDetails dive={selectedDive} />
-    </section>
+    </div>
   );
 });
 
