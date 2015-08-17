cdfe7c7b6ac55325916332dc4e58f8a89d755f5d
Jonas Follesø <jonas@follesoe.no>
2015-08-17 22:41:03 +0200

    Adds build script for nw app
    Build script using nw-builder and gulp

--- build.sh
+++
@@ -1,7 +0,0 @@
-# Assumes you have a copy of nw.js app bundle called SuuntoDMEditor.app in root of repo
-iconutil -c icns main.iconset
-cp main.icns SuuntoDMEditor.app/Contents/Resources
-
-cd src && zip -r ../SuuntoDMEditor.nw *
-cd ..
-cp SuuntoDMEditor.nw SuuntoDMEditor.app/Contents/Resources/app.nw
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var NwBuilder = require('nw-builder');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var shell = require('gulp-shell');
+var del = require('del');
+
+gulp.task('clean', function (cb) {
+  del(['output'], cb);
+});
+
+gulp.task('icons', shell.task([
+  'iconutil -c icns icons/main.iconset -o icons/main.icns'
+]));
+
+gulp.task('nw', ['clean', 'icons'], function () {
+  var nw = new NwBuilder({
+    files: './src/**',
+    buildDir: './output/nwjs',
+    macIcns: './icons/main.icns',
+    platforms: ['osx64']
+  });
+
+  nw.on('log', function (msg) {
+    gutil.log('nw-builder', msg);
+  });
+
+  return nw.build().catch(function (err) {
+    gutil.log('nw-builder', err);
+  });
+});
+
+gulp.task('default', ['nw']);
