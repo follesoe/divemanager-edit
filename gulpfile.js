0328b31efa19a4a0ede9c9351d52141b66bda587
Jonas Follesø <jonas@follesoe.no>
2015-11-08 18:26:53 +0100

    Removes nwjs gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-var NwBuilder = require('nw-builder');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var shell = require('gulp-shell');
@@ -11,23 +10,3 @@ gulp.task('clean', function (cb) {
 gulp.task('icons', shell.task([
   'iconutil -c icns icons/main.iconset -o icons/main.icns'
 ]));
-
-gulp.task('nw', ['clean', 'icons'], function () {
-  var nw = new NwBuilder({
-    version: '0.12.2',
-    files: './src/nwjsapp/**/**',
-    buildDir: './output/nwjs',
-    macIcns: './icons/main.icns',
-    platforms: ['osx64']
-  });
-
-  nw.on('log', function (msg) {
-    gutil.log('nw-builder', msg);
-  });
-
-  return nw.build().catch(function (err) {
-    gutil.log('nw-builder', err);
-  });
-});
-
-gulp.task('default', ['nw']);
