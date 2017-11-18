f40d76bc7a8665ef3fa4eec00c812d9516a3a722
Jonas Follesø <jonas@follesoe.no>
2017-11-18 22:51:58 +0100

    List dives in ascending order

--- src/electronapp/csvexport.js
+++ src/electronapp/csvexport.js
@@ -5,7 +5,7 @@ var dbaccess     = require('./services/dbaccess');
 var pathResult = dbpath.getPath();
 
 dbaccess.getDives(pathResult.path).then(function (dives) {
-  for (var i = 0; i < dives.length; ++i) {
+  for (var i = dives.length - 1; i > 0; --i) {
       console.log(`${dives[i].StartDateTime.format('MM/DD/YY')}, ${dives[i].StartTime}, ${dives[i].Duration}, ${dives[i].MaxDepth}, ${dives[i].AvgDepth}, ${dives[i].ModeType}, ${dives[i].Source}`)
   }
 });
